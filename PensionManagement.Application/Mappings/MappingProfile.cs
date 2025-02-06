3763bc0540dcbe0ca6f214d2a3837706d9c4e834
Bahdtoluwani <oladosu.toluene@gmail.com>
2025-02-06 23:13:59 +0100

    Mappings

---
+++ PensionManagement.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PensionManagement.Domain.Entities;
+using PensionManagement.Application.DTOs;
+
+namespace PensionManagement.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Member, MemberDto>().ReverseMap();
+            
+            // Mapping for Contribution
+            CreateMap<Contribution, ContributionDto>().ReverseMap();
+
+            // Mapping for Benefit
+            CreateMap<Benefit, BenefitDto>().ReverseMap();
+        }
+    }
+}
